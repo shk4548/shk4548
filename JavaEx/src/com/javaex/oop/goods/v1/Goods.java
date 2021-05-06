4a3066876cfdfbfc2bffdec60600249b29dbdb34
shk <shk4548@gmail.com>
2021-05-06 11:54:00 +0900

    method

---
+++ JavaEx/src/com/javaex/oop/goods/v1/Goods.java
@@ -0,0 +1,14 @@
+package com.javaex.oop.goods.v1;
+
+// v2 정보 은닉
+public class Goods {
+	
+	
+	//필드
+	 String name;
+	 int price;
+	// default : 내부 + 동일 패키지
+	
+}
+
+
---
+++ JavaEx/src/com/javaex/oop/goods/v2/Goods.java
@@ -0,0 +1,13 @@
+package com.javaex.oop.goods.v2;
+
+// v1.필드( 데이터 )
+public class Goods {
+	//필드
+	
+	private String name;
+	private int price;
+	
+	
+}
+
+
